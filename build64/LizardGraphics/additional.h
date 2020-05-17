43ad83c895c3b861c732dfcbe59eb8c1ce96ca27
Aleksey <37839016+GercogKaban@users.noreply.github.com>
2020-05-17 14:14:13 +0300

    [DUMMY]

--- build64/LizardGraphics/additional.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "vectors.h"
-
-namespace LGraphics
-{
-    bool isPointInPolygon(int npol, float* xp, float* yp, fvect2 p);
-    fvect2 pointOnDisplayToGlCoords(fvect2 displaySize, fvect2 point);
-}
