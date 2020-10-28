bdbb8eed5316660093b0938637db9e6963d41d36
Aleksey <37839016+GercogKaban@users.noreply.github.com>
2020-10-28 17:46:20 +0200

    Revert "[DUMMY]"
    This reverts commit f5f38ff494a51967f6508adcd112842a11d507fb.

--- LError.cpp
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "LError.h"
-#include "pch.h"
-
-namespace LGraphics
-{
-    std::vector<std::string> LError::errors;
-    size_t LError::lastErrorNum = 0;
-}
--- LTextEdit.cpp
+++
@@ -1,7 +0,0 @@
-#include "LTextEdit.h"
-
-namespace LGraphics
-{
-    LTextEdit::LTextEdit(LApp * app, const std::string text_, const char* path)
-        :LText(app, text_, path) {}
-}
