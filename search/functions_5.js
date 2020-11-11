e67d4a5ece88287648f19ec7833e4cec80d6fd68
GercogKaban <GercogKaban@users.noreply.github.com>
2020-11-11 10:25:44 +0000

    deploy: 96b2580fd04c388ce1ccd76eeca756b71452cdec

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['init_183',['init',['../class_l_graphics_1_1_l_buffer.html#ab4f83135432a6a8c5fa2f7e27468d3c9',1,'LGraphics::LBuffer::init()'],['../class_l_graphics_1_1_l_error.html#abc7934d318f36bfa176ebf35b6b2840f',1,'LGraphics::LError::init()']]],
+  ['init_183',['init',['../class_l_graphics_1_1_l_buffer.html#ab4f83135432a6a8c5fa2f7e27468d3c9',1,'LGraphics::LBuffer::init()'],['../class_l_graphics_1_1_l_error.html#a81734fd1df13f5a68c7ecd77b06b233f',1,'LGraphics::LError::init()']]],
   ['ishidden_184',['isHidden',['../class_l_graphics_1_1_l_widget.html#ac9ec3943fb1eeab1188f9bbc7bcf0150',1,'LGraphics::LWidget']]],
   ['istextureturnedon_185',['isTextureTurnedOn',['../class_l_graphics_1_1_l_image.html#ac5dd290a7783ff1b7e0670555249b7ee',1,'LGraphics::LImage']]]
 ];
