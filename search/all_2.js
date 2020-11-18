893d2b5c9cb1f5c48c5c8fb69455ad0fad9b0823
GercogKaban <GercogKaban@users.noreply.github.com>
2020-11-18 23:43:10 +0000

    deploy: 82ffa07d4399ab90470dfaa592cd66ff2455c4f6

--- search/all_2.js
+++ search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['character_3',['Character',['../struct_l_graphics_1_1_l_line_1_1_character.html',1,'LGraphics::LLine']]],
+  ['character_3',['Character',['../struct_l_graphics_1_1_l_text_render_1_1_character.html',1,'LGraphics::LTextRender']]],
   ['clickfunction_4',['clickFunction',['../class_l_graphics_1_1_l_i_button.html#a5390f7b68aa83a94a8fe685caaaa69fb',1,'LGraphics::LIButton']]],
   ['codegen_5',['CodeGen',['../class_code_gen.html',1,'']]],
   ['color_6',['color',['../class_l_graphics_1_1_l_color_bar.html#a49a08f5b3248acf4a427c082ba283909',1,'LGraphics::LColorBar::color(const unsigned char r, const unsigned char g, const unsigned char b) override'],['../class_l_graphics_1_1_l_color_bar.html#ad356b9b0efda919016dfd46ac35931d9',1,'LGraphics::LColorBar::color(const fvect3 val) override'],['../class_l_graphics_1_1_l_shape.html#a1aa51addad75da57a40bedee25e270c2',1,'LGraphics::LShape::color(const fvect3 val) override'],['../class_l_graphics_1_1_l_shape.html#a555ddfa4f53795f3774194c9ace66239',1,'LGraphics::LShape::color(const unsigned char r, const unsigned char g, const unsigned char b) override'],['../class_l_graphics_1_1_l_widget.html#aa0a7421ebf742faf9a327ec8443c82e2',1,'LGraphics::LWidget::color(const unsigned char r, const unsigned char g, const unsigned char b)=0'],['../class_l_graphics_1_1_l_widget.html#ab2204bd217bfdf26bffad55ac6f5e10e',1,'LGraphics::LWidget::color(const fvect3 val)=0']]],
--- search/classes_0.js
+++ search/classes_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['character_132',['Character',['../struct_l_graphics_1_1_l_line_1_1_character.html',1,'LGraphics::LLine']]],
+  ['character_132',['Character',['../struct_l_graphics_1_1_l_text_render_1_1_character.html',1,'LGraphics::LTextRender']]],
   ['codegen_133',['CodeGen',['../class_code_gen.html',1,'']]],
   ['color_5fdata_134',['color_data',['../structcolor__data.html',1,'']]]
 ];
